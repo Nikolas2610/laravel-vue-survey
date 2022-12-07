3de35258da82a39605939bfe74849eab4c343b98
Psyllou <psyllou@excmgmt.com>
2022-12-07 16:34:52 +0200

    Create dashboard

--- vue-survey/src/store/index.js
+++ vue-survey/src/store/index.js
@@ -154,6 +154,12 @@ const store = createStore({
             data: {},
             token: sessionStorage.getItem("TOKEN"),
         },
+        dashboard: {
+            loading: false,
+            data: {
+
+            }
+        },
         currentSurvey: {
             loading: false,
             data: {
@@ -173,6 +179,20 @@ const store = createStore({
     },
     getters: {},
     actions: {
+        getDashboardData({ commit }) {
+            commit('dashboardLoading', true);
+            return axiosClient.get('/dashboard')
+                .then((res) => {
+                    console.log(res)
+                    commit('dashboardLoading', false);
+                    commit('setDashboardData', res.data);
+                    return res;
+                })
+                .catch(error => {
+                    commit('dashboardLoading', false);
+                    return error;
+                })
+        },
         getSurveyBySlug({ commit }, slug) {
             commit("setCurrentSurveyLoading", true);
             return axiosClient
@@ -257,6 +277,12 @@ const store = createStore({
         }
     },
     mutations: {
+        dashboardLoading(state, loading) {
+            state.dashboard.loading = loading;
+        },
+        setDashboardData(state, data) {
+            state.dashboard.data = data;
+        },
         setCurrentSurveyLoading(state, loading) {
             state.currentSurvey.loading = loading;
         },
